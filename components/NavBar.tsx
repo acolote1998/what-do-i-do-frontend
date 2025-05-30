de2e0ac7447d3eba69ffa85a1b35388506aef48e
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-30 15:48:01 +0200

    initialising navbar's body

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,5 +1,15 @@
 const NavBar = () => {
-  return <div style={{ backgroundColor: "var(--navbar-bg" }}>NavBar</div>;
+  return (
+    <div
+      style={{
+        borderTop: "1px solid var(--navbar-border)",
+        backgroundColor: "var(--navbar-bg)",
+      }}
+      className="h-[8.7vh] w-[100vw] fixed bottom-0"
+    >
+      NavBar
+    </div>
+  );
 };
 
 export default NavBar;
