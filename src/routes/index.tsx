cd9b979ae713ca6b831c3deb0a953499a075c819
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-30 16:22:32 +0200

    initialising main route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,24 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
-} from "@clerk/clerk-react";
-
 export const Route = createFileRoute("/")({
   component: Index,
 });
 
 function Index() {
-  return (
-    <div className="p-2">
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
-  );
+  return <div className="p-2"></div>;
 }
