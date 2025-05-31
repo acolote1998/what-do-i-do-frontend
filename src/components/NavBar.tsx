c5dc645c32d7c89745365e6fa430e33f75ce31cc
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-31 12:18:19 +0200

    adding navbar navigation links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,9 @@
+import { useNavigate } from "@tanstack/react-router";
 import Home from "./icons/Home";
 import New from "./icons/New";
 import Profile from "./icons/Profile";
 const NavBar = () => {
+  const navigate = useNavigate();
   return (
     <div
       style={{
@@ -10,12 +12,29 @@ const NavBar = () => {
       }}
       className="z-50 h-[8.7vh] w-[100vw] fixed bottom-0 flex items-center justify-evenly text-center"
     >
-      <div className="cursor-pointer flex flex-col mt-1 items-center text-center opacity-100">
+      <div
+        onClick={() => {
+          navigate({ to: "/" });
+        }}
+        className="cursor-pointer flex flex-col mt-1 items-center text-center opacity-100"
+      >
         <Home width="4.3vh" />
         <p>Home</p>
       </div>
-      <New className="cursor-pointer" width="7.75vh" opacity="100%" />
-      <div className="cursor-pointer flex flex-col mt-1 items-center text-center opacity-100">
+      <New
+        onClick={() => {
+          navigate({ to: "/create" });
+        }}
+        className="cursor-pointer"
+        width="7.75vh"
+        opacity="100%"
+      />
+      <div
+        onClick={() => {
+          navigate({ to: "/profile" });
+        }}
+        className="cursor-pointer flex flex-col mt-1 items-center text-center opacity-100"
+      >
         <Profile width="5.15vh" />
         <p>Profile</p>
       </div>
