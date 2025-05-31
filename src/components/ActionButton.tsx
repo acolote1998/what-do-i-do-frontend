6c64f7d9e1a7d6bb9fd8d0da24bede1a17accdd0
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-31 11:33:58 +0200

    styling buttons better on hover

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import type { ActionButtonType } from "../types/types";
 
 const ActionButton = ({ heightvh, widthvw, title }: ActionButtonType) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const styles = {
+    width: `${widthvw}vw`,
+    height: `${heightvh}vh`,
+    border: `1px solid var(${isHovered ? "--main-button-border-hover" : "--main-button-border"})`,
+    color: `var(${isHovered ? "--main-button-text-hover" : "--main-button-text"})`,
+    backgroundColor: `var(${isHovered ? "--main-button-hover" : "--main-button"})`,
+    transition: "background-color 0.3s, border-color 0.3s, color 0.3s",
+  };
+
   return (
     <div
-      style={{
-        border: "1px solid var(--main-button-border)",
-        color: "var(--main-button-text)",
-        backgroundColor: "var(--main-button)",
-        width: `${widthvw}vw`,
-        height: `${heightvh}vh`,
-      }}
-      className="font-bold tracking-wider rounded-lg flex items-center justify-center align-middle"
+      style={styles}
+      className="cursor-pointer font-bold tracking-wider rounded-lg flex items-center justify-center align-middle"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
       {title}
     </div>
