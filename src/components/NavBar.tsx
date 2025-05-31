e736ba364ee8162f5fb7740931f6bc76642c3533
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-31 11:35:18 +0200

    changing cursor of the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,12 +10,12 @@ const NavBar = () => {
       }}
       className="h-[8.7vh] w-[100vw] fixed bottom-0 flex items-center justify-evenly text-center"
     >
-      <div className="flex flex-col mt-1 items-center text-center opacity-100">
+      <div className="cursor-pointer flex flex-col mt-1 items-center text-center opacity-100">
         <Home width="4.3vh" />
         <p>Home</p>
       </div>
-      <New width="7.75vh" opacity="100%" />
-      <div className="flex flex-col mt-1 items-center text-center opacity-100">
+      <New className="cursor-pointer" width="7.75vh" opacity="100%" />
+      <div className="cursor-pointer flex flex-col mt-1 items-center text-center opacity-100">
         <Profile width="5.15vh" />
         <p>Profile</p>
       </div>
