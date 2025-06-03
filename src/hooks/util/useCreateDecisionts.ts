53cf9446cf9bfcde52b5f79177eff30187c9f403
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-06-03 19:01:43 +0200

    creating useCreateDecisions hook

---
+++ src/hooks/util/useCreateDecisionts.ts
@@ -0,0 +1,21 @@
+import { useAuth } from "@clerk/clerk-react";
+import axios, { HttpStatusCode } from "axios";
+import type { DecisionsType } from "../../types/types";
+
+const url = "http://localhost:8080/decisions";
+
+export function useCreateDecisions(decision: DecisionsType) {
+  const { getToken } = useAuth();
+
+  const createDecision = async (): Promise<DecisionsType> => {
+    const response = await axios.post(`${url}`, decision, {
+      headers: { Authorization: `Bearer ${await getToken()}` },
+    });
+    if (response.status === HttpStatusCode.Ok) {
+      return response.data;
+    }
+    throw new Error("Could not create the decision");
+  };
+
+  return { createDecision };
+}
