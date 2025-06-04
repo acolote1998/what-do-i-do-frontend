bf335402d8049fda4146fcf37c910915092d2b97
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-06-04 16:53:47 +0200

    makigng cursor to be default when hovering the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,7 @@ const Header = () => {
         borderBottom: "1px solid var(--divs-border)",
         backgroundColor: "var(--divs-bg)",
       }}
-      className="z-50 cursor-pointer h-[8.7vh] w-[100vw] fixed top-0 flex items-center justify-evenly text-center"
+      className="cursor-default z-50 h-[8.7vh] w-[100vw] fixed top-0 flex items-center justify-evenly text-center"
     >
       <h1 style={{ color: "var(--divs-text" }} className="text-4xl">
         What Do I Do?
